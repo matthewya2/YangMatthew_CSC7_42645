2b39a8f9dae337a478c7b5380c74d91fecef71e5
rcc <rcc>
2017-04-18 13:51:24 -0700

    Signed-off-by: rcc <rcc>

--- Mastermind/main.cpp
+++ Mastermind/main.cpp
@@ -31,27 +31,38 @@ int main(int argc, char** argv) {
     int rightNP=0;                  //right number and place
     int rightN=0;                   //right number but not place
     const int MAXTURN=10;           //max turn value
-    cout <<"For this MasterMind game, the computer will select 4 numbers 1-8 "<<endl<<
+    cout <<"For this MasterMind game, the computer will select 4 unique numbers 1-8 "<<endl<<
             "and you must guess which numbers they are, and what order those"<<endl<<
-            " numbers are in within 10 turns."<<endl;
+            "numbers are in within 10 turns."<<endl;
     
-    do{
-        for(int i=0;i<=3;i++){
-        picks[i]=menu.Rand();
+    do{                                 
+        for(int i=0;i<=3;i++){          //makes random code for user to break
+            picks[i]=menu.Rand();
+        }
+        
+        for(int i=0;i<=3;i++){ 
+            while (picks[i]==picks[1]){     //makes the code contain unique numerals
+            picks[i]=menu.Rand();}
+
+            while (picks[i]==picks[2]){
+            picks[i]=menu.Rand();}
+
+            while (picks[i]==picks[3]){
+            picks[i]=menu.Rand();}
         }
         
         for(int i=0;i<=3;i++){
         cout<<picks[i];
         }
-        
-        for(int turnC=0;turnC<MAXTURN;turnC++){
+        cout<<endl;
+        for(int turnC=1;turnC<=MAXTURN;turnC++){
     cout << "what numbers do you want to guess?"<<endl;
     cin>>choice;
-    choice1=choice%10;
-    choice2=choice%100-choice1;
-    choice3=choice%1000-(choice1+choice2);
-    choice4=choice-(choice1+choice2+choice3);
-        
+    choice4=choice%10;
+    choice3=(choice%100-choice1);
+    choice2=choice%1000-(choice1+choice2);
+    choice1=choice-(choice1+choice2+choice3);
+    cout <<choice1<<choice2<<choice3<<choice4<<endl;
         if(choice1==picks[0]){rightNP++;}
         if(choice2==picks[1]){rightNP++;}
         if(choice3==picks[2]){rightNP++;}
